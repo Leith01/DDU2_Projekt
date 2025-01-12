4f559cefee11f9003d0d611d955ac9c3506aea45
Leith Ahmad <leith.216@gmail.com>
2025-01-12 06:12:48 +0100

    js kod för att ändra cellens bakgrundfärg vid klick ha en annan mouseover färg åt den klickade cellen

--- clear/index.js
+++ clear/index.js
@@ -1,13 +1,26 @@
 const fillCleared = document.getElementById("fillCleared");
 
 gridContainer.addEventListener("mouseover", function(event) {
-    if (event.target.className === "grid-cell") {
+    if (event.target.className === "grid-cell"  && event.target.style.backgroundColor !== "red") {
         event.target.style.backgroundColor = "lightblue";
+    } else if (event.target.style.backgroundColor === "red") {
+        event.target.style.backgroundColor = "orange";
     }
 });
 
 gridContainer.addEventListener("mouseout", function(event) {
     if (event.target.className === "grid-cell") {
-        event.target.style.backgroundColor = "";
+        if (event.target.style.backgroundColor === "lightblue") {
+            event.target.style.backgroundColor = ""; 
+        } else if (event.target.style.backgroundColor === "orange") {
+            event.target.style.backgroundColor = "red";
+        }
+    }
+});
+
+gridContainer.addEventListener("click", function(event) {
+    if (event.target.className === "grid-cell") {
+        event.target.style.backgroundColor = "red";
+        event.target.textContent = "";
     }
 });
