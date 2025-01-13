9f95e5d3e8aa5d5fd6805b7f62098d75942a2cec
Leith Ahmad <leith.216@gmail.com>
2025-01-13 01:07:44 +0100

    Logik för att när man klickar på en anna cell återställs de andra kopiorna textContent

--- findSame/index.js
+++ findSame/index.js
@@ -1,5 +1,17 @@
+let previouslyClickedValue = null;
+
 gridContainer.addEventListener("click", function(event) {
     if (event.target && event.target.getAttribute("class") === "grid-cell") {
+
+        if (previouslyClickedValue !== null) {
+            const allCells = gridContainer.querySelectorAll(".grid-cell");
+            for (let cell of allCells) {
+                if (cell.textContent  === previouslyClickedValue) {
+                    cell.style.backgroundColor = "";
+                }
+            }
+        }
+
         const cellValue = event.target.textContent;
 
         const allCells = gridContainer.querySelectorAll(".grid-cell");
@@ -8,5 +20,7 @@ gridContainer.addEventListener("click", function(event) {
                 cell.style.backgroundColor = "lightgreen";
             }
         }
+
+        previouslyClickedValue = cellValue;
     }
 });
