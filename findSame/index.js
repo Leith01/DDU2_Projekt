73bace82e5355beca35493d15fb9687ca5c048a4
Leith Ahmad <leith.216@gmail.com>
2025-01-13 01:19:43 +0100

    Logik för reset button och la till information till min p-element

--- findSame/index.js
+++ findSame/index.js
@@ -1,3 +1,5 @@
+const showCopies = document.getElementById("showCopies");
+const resetButton = document.getElementById("ResetButton");
 let previouslyClickedValue = null;
 
 gridContainer.addEventListener("click", function(event) {
@@ -15,12 +17,25 @@ gridContainer.addEventListener("click", function(event) {
         const cellValue = event.target.textContent;
 
         const allCells = gridContainer.querySelectorAll(".grid-cell");
+        let copyCount = 0;
         for (let cell of allCells) {
             if (cell.textContent === cellValue) {
                 cell.style.backgroundColor = "lightgreen";
+                copyCount++;
             }
         }
 
+        showCopies.textContent = `${copyCount} ${copyCount === 1 ? "copy" : "copies"} of the number ${cellValue}`;
         previouslyClickedValue = cellValue;
     }
+});
+
+resetButton.addEventListener("click", function() {
+    const allCells = gridContainer.querySelectorAll(".grid-cell");
+    for (let cell of allCells) {
+        cell.style.backgroundColor = "";
+    }
+
+    showCopies.textContent = "Click on a number to find copies";
+    previouslyClickedValue = null;
 });
